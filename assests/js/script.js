c641f5cf447cbe9e0e1f182939be35695faeddbb
Gowri4622 <gowrihifi@gmail.com>
2023-03-21 10:46:41 +0530

    Added js file.

---
+++ assests/js/script.js
@@ -0,0 +1,40 @@
+// let name=1
+// console.log("hi hello "+name);
+// name=3;
+// console.log("hi hello "+name);
+
+
+// const fname=1
+// console.log("hi hello "+fname);
+// fname=5
+// console.log("hi hello "+fname);
+
+// var a=10
+// var b=20
+// var c=a-b
+// console.log(c);
+
+// let b=2
+// var c="k"
+// console.log(b===c);
+
+// let b=2
+// let c=3
+// console.log(b==2&&c<2)
+
+let year=1988
+if(year%4==0){
+    console.log("Leap year")
+}
+else if(year%400==0){
+    console.log("Leap year")
+}
+else if(year%100==0){
+    console.log("Not a Leap year")
+}
+else{
+    console.log("Not a Leap year")
+}
+
+
+
